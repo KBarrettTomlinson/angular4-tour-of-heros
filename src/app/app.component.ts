8d46b3ec9a9750193f6a2784fc61b718af58985c
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-09-09 10:58:49 -0500

    adds template craeates hero class in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: ` <h1>{{title}}</h1>
+              <h2>{{hero.name}} details!</h2>
+              <h3>Super Secret ID: {{hero.id}}`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'WindStorm'
+  };
+}
+export class Hero {
+  id: number;
+  name: string;
 }
